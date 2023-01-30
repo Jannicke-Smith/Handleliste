d36bdd6ffbd6449e697faff070fb8f495d9e9863
Jannicke-Smith <114487881+Jannicke-Smith@users.noreply.github.com>
2023-01-30 11:19:33 +0100

    data

---
+++ js/data.js
@@ -0,0 +1,23 @@
+// arrays
+
+const employees = [
+    {
+        person: {
+            firstName: 'Jannicke',
+            lastName: 'Stephansen-Smith',
+            age: 34,
+            dob: "1988-09-18",
+            nationality: 'Norwegian',
+        },
+        department:[ 'HR', 'Sales',],
+        adress:{
+            street: 'Håkaleina',
+            streetNumber: 18,
+            city: 'Slemmestad',
+        },
+        email: 'jannicke_stephansen@hotmail.com',
+    }
+]
+
+
+console.log
